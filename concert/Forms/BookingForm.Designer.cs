eeb64d8506379a7e82d3eff2faf67ba890113ad6
synd4772 <doe481157@gmail.com>
2025-05-02 16:49:20 +0300

    h

--- concert/Forms/BookingForm.Designer.cs
+++ concert/Forms/BookingForm.Designer.cs
@@ -85,9 +85,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(203, 171);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(30, 13);
             this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
+            this.label2.Text = "Date";
             // 
             // BookingForm
             // 
